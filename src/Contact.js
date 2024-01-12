0c6e391f07428324fa6ef835366bc81218014b4a
Coder-pipo <pragnabiswas2001@gmail.com>
2024-01-12 13:01:12 +0530

    Adding Error Page

--- src/Contact.js
+++ src/Contact.js
@@ -1,25 +1,10 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from "react"; 
+import Header from './Component/Header'
 
 function Contact(){
      return (
        <div>
-         <header>
-           <a href="#">Logo</a>
-           <nav>
-             <ul>
-               <li>
-                 <NavLink to="/home">Home</NavLink>
-               </li>
-               <li>
-                 <NavLink to="/about">About</NavLink>
-               </li>
-               <li>
-                 <NavLink to="/contact">Contact</NavLink>
-               </li>
-             </ul>
-           </nav>
-         </header>
+         <Header />
          <section>
            <h1>Contact Page</h1>
          </section>
--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,11 @@
 
 import React from 'react'
-import { NavLink } from "react-router-dom";
+import Header from './Component/Header'
 
 function Home(){
      return (
        <div>
-         <header>
-           <a href="#">Logo</a>
-           <nav>
-             <ul>
-               <li>
-                 <NavLink to="/home">Home</NavLink>
-               </li>
-               <li>
-                 <NavLink to="/about">About</NavLink>
-               </li>
-               <li>
-                 <NavLink to="/contact">Contact</NavLink>
-               </li>
-             </ul>
-           </nav>
-         </header>
+         <Header/>
        </div>
      );
 }
